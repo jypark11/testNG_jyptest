edfb0fd8d77b387b9962366dd2fd9b4821fdf870
jypark11 <jiyong.park@quantil.com>
2023-10-24 01:52:30 -0700

    second commit

--- src/gitTest.java
+++ src/gitTest.java
@@ -3,6 +3,7 @@ public class gitTest {
     //add commit 2nd
     public void mmaster{
         System.out.println("마스터");
+        System.out.println("마스터도 개발을 하는 중이였습니다.");
     }
     public void brancher{}
 }
