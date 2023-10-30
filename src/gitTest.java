2d6c62d35c592e9602709bca539dd099fe63516c
jypark11 <jiyong.park@quantil.com>
2023-10-29 23:45:33 -0700

    commit localMaster to master

--- src/gitTest.java
+++ src/gitTest.java
@@ -1,11 +1,12 @@
 public class gitTest {
     //1st commit
     //add commit 2nd
-    public void mmaster{
-        System.out.println("마스터");
-        System.out.println("마스터도 개발을 하는 중이였습니다.");
+    public void mmaster(){
+        System.out.println("1.마스터");
+        System.out.println("2. 마스터도 개발을 하는 중이였습니다.");
+        System.out.println("3. 여기까지 마스터와 지용이 같은 내용입니다. ");
     }
-    public void brancher{
-        System.out.println("제가 수정해 볼께요.");
+    public void brancher(){
+
     }
 }
---
+++ src/git_master_jiyong_sync_test.java
@@ -0,0 +1,4 @@
+public class git_master_jiyong_sync_test {
+    //1. LocalMaster--->push to master
+    //2. Master--->Localjiyong.
+}
